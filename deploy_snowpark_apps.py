9f0665637b20fb68ffb4269da254298f7135d20a
andekarl73 <andekar@yahoo.com>
2024-11-08 16:23:39 +0100

    Check snow connection

--- deploy_snowpark_apps.py
+++ deploy_snowpark_apps.py
@@ -50,6 +50,7 @@ for (directory_path, directory_names, file_names) in os.walk(root_directory):
     # Make sure all 6 SNOWFLAKE_ environment variables are set
     # SnowCLI accesses the passowrd directly from the SNOWFLAKE_PASSWORD environmnet variable
     os.system("snow --version")
+    os.system("snow connection list")
 
     #cmd = "snow snowpark build"
 '''  try:
