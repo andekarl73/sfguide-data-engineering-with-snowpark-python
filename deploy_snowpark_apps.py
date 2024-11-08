60f0d64c4d0a8e66152f21654fda74a781042d86
andekarl73 <andekar@yahoo.com>
2024-11-08 15:45:24 +0100

    Test pip snowflake-cli

--- deploy_snowpark_apps.py
+++ deploy_snowpark_apps.py
@@ -49,6 +49,7 @@ for (directory_path, directory_names, file_names) in os.walk(root_directory):
     print(f"folder of function/proc '{directory_path}")
     # Make sure all 6 SNOWFLAKE_ environment variables are set
     # SnowCLI accesses the passowrd directly from the SNOWFLAKE_PASSWORD environmnet variable
+    os.system("pip install snowflake-cli")
     cmd = "snow snowpark build"
     try:
         os.system(cmd)
